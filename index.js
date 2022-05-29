d352e738e74e7413c3e20ff78d41504efd2d80f0
shariarhossain23e <shariarhossain23@gamil.comm>
2022-05-30 00:59:22 +0600

    user put api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
+var jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port =process.env.PORT || 5000
 
@@ -22,8 +23,25 @@ client.connect(err => {
 async function run() {
     try {
       await client.connect();
-      const database = client.db("Blogs").collection("user");
-      
+      const blogsUser = client.db("Blogs").collection("user");
+
+      app.put('/user/:email',async(req,res)=>{
+          const email = req.params.email;
+          const user = req.body;
+          const filter = {email:email}
+          const options = { upsert: true };
+          const updateDoc = {
+            $set: user,
+          };
+
+          const result = await blogsUser.insertOne(filter,updateDoc,options);
+          const token = jwt.sign({ email:email }, process.env.JWT_TOKEN, 
+            { expiresIn: '1h' }
+          )
+
+          res.send({result , token})
+          
+      })
      
     } finally {
     
