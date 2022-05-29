a2ab6c8272bd60898730c27a8ee83246dc52c37e
shariarhossain23e <shariarhossain23@gamil.comm>
2022-05-30 02:01:45 +0600

    generate token

--- index.js
+++ index.js
@@ -15,11 +15,7 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.plsay.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log("db connect");
-  client.close();
-});
+
 async function run() {
     try {
       await client.connect();
@@ -34,7 +30,7 @@ async function run() {
             $set: user,
           };
 
-          const result = await blogsUser.insertOne(filter,updateDoc,options);
+          const result = await blogsUser.updateOne(filter,updateDoc,options);
           const token = jwt.sign({ email:email }, process.env.JWT_TOKEN, 
             { expiresIn: '1h' }
           )
